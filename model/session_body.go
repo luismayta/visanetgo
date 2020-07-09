5154d25cd1c33db7be0c32edd82911b73a9d7fdd
@slovacus <slovacus@gmail.com>
2020-07-09 18:57:13 -0500

    refactor: format syntax code (#7)

--- model/session_body.go
+++ model/session_body.go
@@ -1,20 +1,23 @@
 package model
 
+// SessionBody struct.
 type SessionBody struct {
-  Amount              float64        `json:"amount"`
-  Antifraud           Antifraud     `json:"antifraud"`
-  Channel             string        `json:"channel"`
-  RecurrenceMaxAmount *float64       `json:"recurrenceMaxAmount"`
+	Amount              float64   `json:"amount"`
+	Antifraud           Antifraud `json:"antifraud"`
+	Channel             string    `json:"channel"`
+	RecurrenceMaxAmount *float64  `json:"recurrenceMaxAmount"`
 }
 
+// Antifraud struct.
 type Antifraud struct {
-  ClientIP           string              `json:"clientIp"`
-  MerchantDefineData  MerchantDefineData   `json:"merchantDefineData"`
+	ClientIP           string             `json:"clientIp"`
+	MerchantDefineData MerchantDefineData `json:"merchantDefineData"`
 }
 
+// MerchantDefineData struct.
 type MerchantDefineData struct {
-  Mdd4    string  `json:"MDD4"`
-  Mdd32   string  `json:"MDD32"`
-  Mdd75   string  `json:"MDD75"`
-  Mdd77   int     `json:"MDD77"`
+	Mdd4  string `json:"MDD4"`
+	Mdd32 string `json:"MDD32"`
+	Mdd75 string `json:"MDD75"`
+	Mdd77 int    `json:"MDD77"`
 }
