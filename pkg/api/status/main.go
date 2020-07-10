20638446c5d94793fde121df267a1e4b7a31df83
@slovacus <slovacus@gmail.com>
2020-07-09 19:07:16 -0500

    chore: implement status for api (#7)

---
+++ pkg/api/status/main.go
@@ -0,0 +1,16 @@
+package status
+
+const (
+	// Ok status response 200
+	Ok = 200
+	// Created status response 201
+	Created = 201
+	// Accepted status response 202
+	Accepted = 202
+	// BadRequest status response 400
+	BadRequest = 400
+	// Unauthorized status response 401
+	Unauthorized = 401
+	// UnauthorizedAccessTokenExpired status response 460
+	UnauthorizedAccessTokenExpired = 460
+)
